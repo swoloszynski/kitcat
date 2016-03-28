d91df8b0e10d8eb0ec54b2ad85f8424970b21329
Sam Woloszynski <sam.w@virginia.edu>
2016-03-28 01:40:05 -0400

    Add basic admin

--- kitcatapp/admin.py
+++ kitcatapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Contact, Connection
 
-# Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': [('first_name', 'last_name', 'frequency'),
+                           ('phone', 'email', 'address')]}),
+        ('History', {'fields': [('date_met', 'how_met'), 'about'],
+                     'classes': ['collapse']}),
+    ]
+
+admin.site.register(Contact, ContactAdmin)
+admin.site.register(Connection)
