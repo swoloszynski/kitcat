3a6c0beef14eb2b1f88962794c53276f61c2f8ea
Sam Woloszynski <sam.w@virginia.edu>
2016-03-28 00:03:48 -0400

    Add unit tests for Contact and Connection models

--- kitcatapp/tests.py
+++ kitcatapp/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from kitcatapp.models import Contact, Connection
+from django.utils import timezone
+from datetime import datetime
 
-# Create your tests here.
+# Models
+class ContactTest(TestCase):
+
+    def test_contact_str(self):
+        contact = Contact(first_name='Marilyn', \
+                            last_name='Monroe', \
+                            frequency = '180')
+        self.assertTrue(isinstance(contact, Contact))
+        self.assertEqual(contact.__str__(), 'Marilyn Monroe')
+
+class ConnectionTest(TestCase):
+
+    def test_connection_str(self):
+        contact = Contact.objects.create(first_name='Marilyn', \
+                            last_name='Monroe', \
+                            frequency = '180')
+
+        connection = Connection(contact=contact, \
+                                is_complete=False,
+                                due_date=datetime(2016, 3, 26))
+
+        self.assertTrue(isinstance(connection, Connection))
+        self.assertEqual(connection.__str__(), 'Marilyn Monroe 2016-03-26')
