3ee70208862f794d5ce3234c9c24aa049e0479e4
Sam Woloszynski <sam.w@virginia.edu>
2016-03-28 02:38:21 -0400

    Add /static to urls

--- kitcat/urls.py
+++ kitcat/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'kitcat.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+)
