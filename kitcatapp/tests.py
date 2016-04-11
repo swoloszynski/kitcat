19d6cd5ef329284664e06d1ecd29771c8ad23951
Sam Woloszynski <sam.w@virginia.edu>
2016-04-11 02:28:07 -0400

    Refactor get_reminders

--- kitcatapp/tests.py
+++ kitcatapp/tests.py
@@ -103,12 +103,126 @@ class ProfileTest(TestCase):
 # Commands
 class BasicGetRemindersTest(TestCase):
     fixtures = ['users', 'profiles', 'contacts', 'connections']
+
+    def test_format_message_single_due_connection(self):
+        command = Command()
+        due_date = datetime.date(2016, 3, 19)
+        connections = Connection.objects.get(due_date=due_date)
+        message = Command._format_message(command, [connections], due_date)
+        expected_message = 'Call Amy Schumer!'
+        self.assertEqual(message, expected_message)
+
+    def test_format_message_single_overdue_connection(self):
+        command = Command()
+        connection_due_date = datetime.date(2016, 3, 19)
+        check_due_date = datetime.date(2016, 3, 20)
+        connections = Connection.objects.get(due_date=connection_due_date)
+        message = Command._format_message(command, [connections], check_due_date)
+        expected_message = 'Really, call Amy Schumer!'
+        self.assertEqual(message, expected_message)
+
+    def test_format_message_multiple_due_connections(self):
+        command = Command()
+        due_date = datetime.date(2016, 4, 21)
+        connections = Connection.objects.filter(due_date=due_date)
+        message = Command._format_message(command, connections, due_date)
+        expected_message = 'Call Tina Fey!\nCall Amy Schumer!'
+        self.assertEqual(message, expected_message)
+
+    def test_format_message_multiple_overdue_connections(self):
+        command = Command()
+        connection_due_date = datetime.date(2016, 4, 21)
+        check_due_date = datetime.date(2016, 4, 22)
+        connections = Connection.objects.filter(due_date=connection_due_date)
+        message = Command._format_message(command, connections, check_due_date)
+        expected_message = 'Really, call Tina Fey!\nReally, call Amy Schumer!'
+        self.assertEqual(message, expected_message)
+
+    def test_format_message_due_and_overdue_connections(self):
+        command = Command()
+        connection_start_due_date = datetime.date(2016, 3, 28)
+        check_due_date = datetime.date(2016, 3, 29)
+        connections = Connection.objects.filter(due_date__lte=check_due_date) \
+            .filter(due_date__gte=connection_start_due_date) \
+            .order_by('-due_date')
+        message = Command._format_message(command, connections, check_due_date)
+        expected_message = 'Call Amy Schumer!\nReally, call Tina Fey!'
+        self.assertEqual(message, expected_message)
+
+    def test_format_message_future_connections(self):
+        command = Command()
+        due_date = datetime.date(2016, 3, 29)
+        connections = Connection.objects \
+            .filter(due_date__gt=due_date) \
+            .order_by('-due_date')
+        message = Command._format_message(command, connections, due_date)
+        expected_message = ''
+        self.assertEqual(message, expected_message)
+
+    def test_get_connections_no_connections_for_user(self):
+        command = Command()
+        user = 3
+        due_date = datetime.date(2016, 3, 29)
+        connections = Command._get_connections(command, user, due_date)
+        self.assertEqual(len(connections), 0)
+
+    def test_get_connections_only_future(self):
+        command = Command()
+        user = 1
+        due_date = datetime.date(2014, 3, 29)
+        connections = Command._get_connections(command, user, due_date)
+        self.assertEqual(len(connections), 0)
+
+    def test_get_connections_single(self):
+        command = Command()
+        user = 1
+        due_date = datetime.date(2016, 3, 19)
+        connections = Command._get_connections(command, user, due_date)
+        self.assertEqual(len(connections), 1)
+
+        contact = Contact.objects.get(first_name='Amy')
+        expected_conn = Connection(contact=contact, \
+                            is_complete=False,
+                            due_date=datetime.date(2016, 3, 19))
+        first_conn = connections[0]
+        self.assertEqual(first_conn.contact, expected_conn.contact)
+        self.assertEqual(first_conn.is_complete, expected_conn.is_complete)
+        self.assertEqual(first_conn.due_date, expected_conn.due_date)
+        self.assertEqual(first_conn.notes, expected_conn.notes)
+
+    def test_get_connections_multiple(self):
+        command = Command()
+        user = 1
+        due_date = datetime.date(2016, 3, 21)
+        connections = Command._get_connections(command, user, due_date)
+        self.assertEqual(len(connections), 2)
+
+        contact_1 = Contact.objects.get(first_name='Tina')
+        expected_conn_1 = Connection(contact=contact_1, \
+                            is_complete=False,
+                            due_date=datetime.date(2016, 3, 21))
+        conn_1 = connections[0]
+        self.assertEqual(conn_1.contact, expected_conn_1.contact)
+        self.assertEqual(conn_1.is_complete, expected_conn_1.is_complete)
+        self.assertEqual(conn_1.due_date, expected_conn_1.due_date)
+        self.assertEqual(conn_1.notes, expected_conn_1.notes)
+
+        contact_2 = Contact.objects.get(first_name='Amy')
+        expected_conn_2 = Connection(contact=contact_2, \
+                            is_complete=False,
+                            due_date=datetime.date(2016, 3, 19))
+        conn_2 = connections[1]
+        self.assertEqual(conn_2.contact, expected_conn_2.contact)
+        self.assertEqual(conn_2.is_complete, expected_conn_2.is_complete)
+        self.assertEqual(conn_2.due_date, expected_conn_2.due_date)
+        self.assertEqual(conn_2.notes, expected_conn_2.notes)
+
     def test_date_ok_send_sms_reminder_for_due_connections(self):
         with mock.patch.object(Command, '_send_sms_reminder'):
             call_command('get_reminders', '-y 2016', '-d 19', '-m 03')
             self.assertTrue(Command._send_sms_reminder.called, "Failed to send SMS.")
             expected_to_phone = '+17036257100'
-            expected_reminder_text = 'Call Amy Schumer!\n'
+            expected_reminder_text = 'Call Amy Schumer!'
             Command._send_sms_reminder.assert_called_with(expected_to_phone, expected_reminder_text)
 
     def test_date_ok_send_sms_reminder_for_overdue_connections(self):
@@ -116,7 +230,7 @@ class BasicGetRemindersTest(TestCase):
             call_command('get_reminders', '-y 2016', '-d 20', '-m 03')
             self.assertTrue(Command._send_sms_reminder.called, "Failed to send SMS.")
             expected_to_phone = '+17036257100'
-            expected_reminder_text = 'Really, call Amy Schumer!\n'
+            expected_reminder_text = 'Really, call Amy Schumer!'
             Command._send_sms_reminder.assert_called_once_with(expected_to_phone, expected_reminder_text)
 
     def test_date_ok_send_sms_reminder_for_due_and_overdue_connections(self):
@@ -124,7 +238,7 @@ class BasicGetRemindersTest(TestCase):
             call_command('get_reminders', '-y 2016', '-d 21', '-m 03')
             self.assertTrue(Command._send_sms_reminder.called, "Failed to send SMS.")
             expected_to_phone = '+17036257100'
-            expected_reminder_text = 'Call Tina Fey!\nReally, call Amy Schumer!\n'
+            expected_reminder_text = 'Call Tina Fey!\nReally, call Amy Schumer!'
             Command._send_sms_reminder.assert_called_once_with(expected_to_phone, expected_reminder_text)
 
     def test_date_ok_dont_send_sms_reminder_if_no_connections(self):
@@ -156,11 +270,11 @@ class MultiUserGetRemindersTest(TestCase):
             self.assertEqual(Command._send_sms_reminder.call_count, 2)
 
             expected_to_phone_1 = '+17036257100'
-            expected_reminder_text_1 = 'Call Tina Fey!\n'
+            expected_reminder_text_1 = 'Call Tina Fey!'
             first_call = mock.call(expected_to_phone_1, expected_reminder_text_1)
 
             expected_to_phone_2 = '+1234568790'
-            expected_reminder_text_2 = 'Call Stephen Colbert!\n'
+            expected_reminder_text_2 = 'Call Stephen Colbert!'
             second_call = mock.call(expected_to_phone_2, expected_reminder_text_2)
 
             calls = [first_call, second_call]
